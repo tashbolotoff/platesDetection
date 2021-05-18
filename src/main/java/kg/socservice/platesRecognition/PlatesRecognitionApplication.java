ceaf96968b35a70f990669f08b84cf9268b8c91d
bakai <bakaiks312@gmail.com>
2021-05-18 12:10:24 +0600

    first commit

---
+++ src/main/java/kg/socservice/platesRecognition/PlatesRecognitionApplication.java
@@ -0,0 +1,13 @@
+package kg.socservice.platesRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatesRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatesRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/socservice/platesRecognition/PlatesRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.socservice.platesRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatesRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
